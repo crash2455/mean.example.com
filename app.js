b0699e63bef63c469415d6d77ddd082384f0c9a4
Space Cow <mattcampen@gmail.com>
2019-01-17 14:58:45 -0600

    implemented user js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiUsersRouter = require('./routes/api/users');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/users', apiUsersRouter);
 app.use('/users', usersRouter);
 
 //Connect to MongoDB
