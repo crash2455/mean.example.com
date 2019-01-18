d20519b786f97b5a02f1431c58c0129530598de5
Space Cow <mattcampen@gmail.com>
2019-01-18 16:22:37 -0600

    completed user login

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 var Users = require('./models/users');
 
+var authRouter = require('./routes/auth');
 var apiAuthRouter = require('./routes/api/auth');
 var apiUsersRouter = require('./routes/api/users');
 var indexRouter = require('./routes/index');
@@ -71,6 +72,7 @@ app.use('/', indexRouter);
 app.use('/api/auth', apiAuthRouter);
 app.use('/api/users', apiUsersRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ src/js/auth.app.js
@@ -0,0 +1,74 @@
+var authApp = (function() {
+
+  function loginForm(){
+    let app = document.getElementById('app');
+
+    let form =  `
+      <div class="card login-form">
+        <form id="loginForm" class="card-body">
+          <h1 class="card-title text-center">Please Sign In</h1>
+          <div id="formMsg" class="alert alert-danger text-center">Invalid username or password</div>
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" class="form-control">
+          </div>
+          <div>
+            <input type="submit" value="Sign In" class="btn btn-lg btn-primary btn-block">
+          </div>
+        </form>
+      </div>
+    `;
+
+    app.innerHTML=form;
+  }
+
+  function postRequest(formId, url){
+    let form = document.getElementById(formId);
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+
+      let formData = new FormData(form);
+      let uri = `${window.location.origin}${url}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', uri);
+
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+
+      let object = {};
+      formData.forEach(function(value, key){
+        object[key]=value;
+      });
+
+      xhr.send(JSON.stringify(object));
+      xhr.onload = function(){
+        let data = JSON.parse(xhr.response);
+
+        if(data.success===true){
+          window.location.href = '/';
+        }else{
+          document.getElementById('formMsg').style.display='block';
+        }
+        console.log(data);
+      }
+    });
+  }
+
+  return {
+    load: function(){
+      loginForm();
+      postRequest('loginForm', '/api/auth/login');
+    }
+  }
+
+
+
+})();
+
+authApp.load();
