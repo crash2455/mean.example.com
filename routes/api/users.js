811bd6d810a98d0fac062dd38e8da7bbe1bca836
Space Cow <mattcampen@gmail.com>
2019-01-18 11:24:16 -0600

    added authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,6 +29,41 @@ router.get('/:userId', function(req,res){
 
  });
 
+//~line 32 before routes
+app.use(require('express-session')({
+  //Define the session store
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  }),
+  //Set the secret
+  secret: config.session.secret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    path: '/',
+    domain: config.cookie.domain,
+    //httpOnly: true,
+    //secure: true,
+    maxAge:3600000 //1 hour
+  }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+  done(null,{
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    first_name: user.first_name,
+    last_name: user.last_name
+  });
+});
+
+passport.deserializeUser(function(user, done){
+  done(null, user);
+});
+
  router.post('/', function(req, res) {
 
   Users.create(new Users({
