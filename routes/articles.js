28ad551197e1e885d36a1298d5e8a1b6fdc5335b
Space Cow <mattcampen@gmail.com>
2019-01-22 13:04:40 -0600

    added intial articles api

---
+++ routes/articles.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/app', function(req, res, next) {
+  res.render('articles/app', { title: 'Blog' });
+});
+
+module.exports = router;
